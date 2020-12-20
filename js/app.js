194a28d6e3d7cda8ec6f44f284f596d4357c72ed
Madeirinha <pavlo@ecomadeirinha.com>
2020-12-20 18:47:52 +0000

    corrections from Volodymyr

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 //Get the button:
-var mybutton = document.getElementById("myBtn");
+var btn_to_the_top = document.getElementById("button_to_the_top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
+    if (!btn_to_the_top) { //check if button exists
+        return;
+    }
+
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
+        btn_to_the_top.style.display = "block";
     } else {
-        mybutton.style.display = "none";
+        btn_to_the_top.style.display = "none";
     }
 }
 
